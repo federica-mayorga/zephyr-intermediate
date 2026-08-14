4b64fb6c81273c270d8c68de32cf6a42ef64b662
Federica Mayorga <federica.mayorga@owl-services.com>
2026-08-14 20:13:12 -0300

    l1-task1: three preemptive threads at priorities 7/5/3
    - t_low (7) logs T_LOW running, sleeps 300ms
    - t_med (5) logs T_MED running, sleeps 200ms
    - t_high (3) logs T_HIGH running, sleeps 100ms

--- app/src/main.c
+++ app/src/main.c
@@ -5,27 +5,40 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_A 5
-#define PRIO_B 5
+#define PRIO_L 7
+#define PRIO_M 5
+#define PRIO_H 3
 
-void thread_a_fn(void *p1, void *p2, void *p3)
+void t_low_fn(void *p1, void *p2, void *p3)
 {
     while (1) {
-        k_msleep(200);
-    }
-}
-
-void thread_b_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
+        LOG_INF("T_LOW running");
         k_msleep(300);
     }
 }
 
-K_THREAD_DEFINE(thread_a, STACK_SIZE, thread_a_fn,
-                NULL, NULL, NULL, PRIO_A, 0, 0);
-K_THREAD_DEFINE(thread_b, STACK_SIZE, thread_b_fn,
-                NULL, NULL, NULL, PRIO_B, 0, 0);
+void t_med_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_MED running");
+        k_msleep(200);
+    }
+}
+
+void t_high_fn(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("T_HIGH running");
+        k_msleep(100);
+    }
+}
+
+K_THREAD_DEFINE(thread_low, STACK_SIZE, t_low_fn,
+                NULL, NULL, NULL, PRIO_L, 0, 0);
+K_THREAD_DEFINE(thread_med, STACK_SIZE, t_med_fn,
+                NULL, NULL, NULL, PRIO_M, 0, 0);
+K_THREAD_DEFINE(thread_high, STACK_SIZE, t_high_fn,
+                NULL, NULL, NULL, PRIO_H, 0, 0);
 
 int main(void)
 {
