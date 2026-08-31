99b979cf2ba08b0943132e65bbc23539e178bd56
Federica Mayorga <federica.mayorga@owl-services.com>
2026-08-31 12:07:18 -0300

    l2-task1: two equal-priority threads racing on a shared counter
    - Both threads increment a shared volatile counter ITERATIONS times
    - main joins both and reports counter vs expected
    - CONFIG_TIMESLICE_SIZE=1 for frequent preemption
    - Increments lost on hardware, varying every run

--- app/src/main.c
+++ app/src/main.c
@@ -5,64 +5,28 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
 #define STACK_SIZE 1024
 
-#define PRIO_L 7
-#define PRIO_M 5
-#define PRIO_H 3
-#define PRIO_C (-1)
-#define DELAYED_START_MS 3000
+#define PRIO 5
+#define ITERATIONS 1000000
 
-void t_low_fn(void *p1, void *p2, void *p3)
+static volatile uint32_t counter;
+
+static void worker_fn(void *p1, void *p2, void *p3)
 {
-    while (1) {
-        LOG_INF("T_LOW running");
-        k_msleep(300);
+    for (int i = 0; i < ITERATIONS; i++) {
+        counter++;
     }
 }
 
-void t_med_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_MED running");
-        k_msleep(200);
-    }
-}
-
-void t_high_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("T_HIGH running");
-        k_msleep(100);
-    }
-}
-
-void t_coop_fn(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        for (int i = 0; i < 5; i++) {
-            volatile uint32_t sum = 0;
-
-            for (int j = 0; j < 1000; j++) {
-                sum += j;
-            }
-
-            LOG_INF("T_COOP running, iteration=%d/5", i+1);
-        }
-        k_yield();
-    }
-}
-
-K_THREAD_DEFINE(thread_low, STACK_SIZE, t_low_fn,
-                NULL, NULL, NULL, PRIO_L, 0, 0);
-K_THREAD_DEFINE(thread_med, STACK_SIZE, t_med_fn,
-                NULL, NULL, NULL, PRIO_M, 0, 0);
-K_THREAD_DEFINE(thread_high, STACK_SIZE, t_high_fn,
-                NULL, NULL, NULL, PRIO_H, 0, 0);
-
-K_THREAD_DEFINE(thread_coop, STACK_SIZE, t_coop_fn,
-                NULL, NULL, NULL, PRIO_C, 0, DELAYED_START_MS);
+K_THREAD_DEFINE(thread_a, STACK_SIZE, worker_fn,
+                NULL, NULL, NULL, PRIO, 0, 0);
+K_THREAD_DEFINE(thread_b, STACK_SIZE, worker_fn,
+                NULL, NULL, NULL, PRIO, 0, 0);
 
 int main(void)
 {
+    k_thread_join(thread_a, K_FOREVER);
+    k_thread_join(thread_b, K_FOREVER);
+
+    LOG_INF("Counter: %u  Expected: %u  Lost: %u", counter, ITERATIONS * 2, ITERATIONS * 2 - counter);
     return 0;
 }
-
